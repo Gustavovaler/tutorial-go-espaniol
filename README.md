0ab22077eee163bafd99681f26e915e221951310
gustavo <gustavodevaler@gmail.com>
2020-03-22 17:19:51 -0300

    Readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# Tutorial de Go ( golang) en español
+
+	En este repo encontraras un archivo con ejemplos basicos de los 
+	distintos componentes del lenguaje Go para que de una vista rápida 
+	encuentres lo que estas necesitando.
+
+
+###Aviso:
+	Por algun motivo Github agrega un tabulado de 4 espacios mas de lo que tiene
+	 el archivo original. Estos ejemplos fueron echos siguiendo la guia de estilos
+	 oficial de Go que marca tabulados de 8 espacios, pero Github por algun motivo que no puedo explicar aún los esta llevando a 12 espacios. 
--- slices.go
+++ slices.go
@@ -5,80 +5,80 @@ import (
 )
 
 func main(){
-	//Slices son una de las funciones mas usadas en Go
-	// Aunque guardan una similitud con los arrays tienen caracteristicas distintas
+		//Slices son una de las funciones mas usadas en Go
+		// Aunque guardan una similitud con los arrays tienen caracteristicas distintas
 
-	//definiendo un slice
+		//definiendo un slice
 
-	s := make([]int, 3)
-
-
-	// asignar valores a un slice
-
-	s[0] = 1
-	s[1] = 2
-	s[2] = 3
-
-	//imprimimos el slice completo
-	fmt.Println(s)
-
-	//imprimir elemento especifico de un slice
-	fmt.Println(s[0])
-
-	// la funcion len() puede ser aplicada a un slice
-	fmt.Println(len(s))
-
-	// funcion append() para agregar un registro a slice
-
-	s = append(s, 5, 6)
-
-	fmt.Println(s)
-
-	//Imprimir segmento del slice
-
-	fmt.Println(s[:3]) // imprime desde el 0 hasta el elemento en la posicion 2
-
-	fmt.Println(s[1:3])//Imprime desde la posicion 2 hasta la 3
-
-	fmt.Println(s[2:])//imprime desde a posicion 2 hasta el ultimo elemento del slice
-
-	// definicion y aignacion simultanea de un slice
-
-	t := []int{100, 200, 300}
-
-	fmt.Println(t)
-
-	//copia de un slice
-
-	// esta es la forma incorrecta,descomentar para probar
-
-	//x := s   //copiamos el slice s en x
-	//fmt.Println(x) // Al imprimir pareceria que ssta  todo bien
-	// Pero no es asi, ya que si modificamos u valer en x tambien se cambiara en s
-	//x[0] = 100
-	//fmt.Println(x)
-	//fmt.Println(s) // como podemos ver ambos slices sufrieron el cambio
-
-	// -- FORMA CORRECTA DE COPIAR UN SLICE
-
-	x := make([]int, len(s))
-
-	copy(x, s)
-
-	x[0] = 100
-
-	fmt.Println(x)
-	fmt.Println(s)
+		s := make([]int, 3)
 
 
-	//slices 2d similar a los arrays aunque el largo de los slices puede variar
+		// asignar valores a un slice
 
-	ss := make([][]int, 3)
+		s[0] = 1
+		s[1] = 2
+		s[2] = 3
 
-	for i := 0; i < 3; i++ {
-				fmt.Println(ss[i])
-	} 
-	
+		//imprimimos el slice completo
+		fmt.Println(s)
+
+		//imprimir elemento especifico de un slice
+		fmt.Println(s[0])
+
+		// la funcion len() puede ser aplicada a un slice
+		fmt.Println(len(s))
+
+		// funcion append() para agregar un registro a slice
+
+		s = append(s, 5, 6)
+
+		fmt.Println(s)
+
+		//Imprimir segmento del slice
+
+		fmt.Println(s[:3]) // imprime desde el 0 hasta el elemento en la posicion 2
+
+		fmt.Println(s[1:3])//Imprime desde la posicion 2 hasta la 3
+
+		fmt.Println(s[2:])//imprime desde a posicion 2 hasta el ultimo elemento del slice
+
+		// definicion y aignacion simultanea de un slice
+
+		t := []int{100, 200, 300}
+
+		fmt.Println(t)
+
+		//copia de un slice
+
+		// esta es la forma incorrecta,descomentar para probar
+
+		//x := s   //copiamos el slice s en x
+		//fmt.Println(x) // Al imprimir pareceria que ssta  todo bien
+		// Pero no es asi, ya que si modificamos u valer en x tambien se cambiara en s
+		//x[0] = 100
+		//fmt.Println(x)
+		//fmt.Println(s) // como podemos ver ambos slices sufrieron el cambio
+
+		// -- FORMA CORRECTA DE COPIAR UN SLICE
+
+		x := make([]int, len(s))
+
+		copy(x, s)
+
+		x[0] = 100
+
+		fmt.Println(x)
+		fmt.Println(s)
+
+
+		//slices 2d similar a los arrays aunque el largo de los slices puede variar
+
+		ss := make([][]int, 3)
+
+		for i := 0; i < 3; i++ {
+					fmt.Println(ss[i])
+		} 
+		
 		
 	 
 }
