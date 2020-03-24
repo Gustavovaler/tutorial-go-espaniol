fde73b2b57851197bf5f45b88ff8fe0700c0f670
gustavo <gustavodevaler@gmail.com>
2020-03-24 01:53:25 -0300

    .

--- basico/manejar-archivos/leer-archivo.go
+++ basico/manejar-archivos/leer-archivo.go
@@ -2,17 +2,28 @@ package main
 
 import (
 		"fmt"
-		"io/ioutil"
+		"io/ioutil"// este paquete tiene funciones especificas para amnejo de archivos
 )
 
 func main(){
-
+	// la func ReadFile devuelve 2 valores
+	// el primero son lso datos y el segundo el error si lo hubiera
 	datos,err := ioutil.ReadFile("./muestra.txt")
 
+	// en caso de presentarse un error lo atrapamos en el condicional
 	if err != nil {
 		fmt.Println("Hubo un error")
 	}
+	// para imprimir los datos debemos convertirlos a string
+	fmt.Println(string(datos))	
 
-	fmt.Println(string(datos))	   
+
 	 
 }
+
+// Nota Importante:
+// esta forma de lerr archivos no es la mas eficiente y 
+// solo es recomendad para archivos pequeños ya que 
+// carga el archivo completo en memoria para procesarlo
+// En el siguiente ejemplo refactorizaremos con otro 
+// metodo mas eficiente.
