4d81b61468330ca01ba1f28f245d10b02d101dda
gustavo <gustavodevaler@gmail.com>
2020-03-24 02:13:52 -0300

    Leer archivos 2

---
+++ basico/manejar-archivos/leer-archivo2.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+		"fmt"
+		"bufio" 
+		"os"
+)
+
+func main(){
+
+	// la variable archivo recibe el archivo del paquete OS
+	// como devuelve 2 valores desechamos el segundo valor usando  " _ "
+	archivo,_ := os.Open("./muestra.txt")
+
+	//ahora creamos un objeto scanner
+	scanner := bufio.NewScanner(archivo)
+
+	//iteramos sobre el resultado
+	for scanner.Scan() {
+
+		//el método Text nos devuelve linea por linea
+		linea := scanner.Text()
+		fmt.Println(linea)
+
+		// separamos para ver el resultado mas notoriamente
+		fmt.Println("---------------------")
+	
+	}
+	 
+}
